647bed76f6648f881762e832bc7d721272207748
TomtePain <123365386+TomtePain@users.noreply.github.com>
2023-08-01 12:16:46 +0200

    working on adding user to firebase

--- CRM-BusinessApp/src/app/dialog-add-user/dialog-add-user.component.ts
+++ CRM-BusinessApp/src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { User } from 'src/models/user.class';
+import { inject } from '@angular/core';
+import { Firestore, collectionData, collection, doc, updateDoc, addDoc, setDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -8,14 +11,29 @@ import { User } from 'src/models/user.class';
   templateUrl: './dialog-add-user.component.html',
   styleUrls: ['./dialog-add-user.component.scss']
 })
+
+
 export class DialogAddUserComponent {
+
+  firestore: Firestore = inject(Firestore);
+  items$: Observable<any> | undefined;
+
+  
+  constructor() {
+    const itemCollection = collection(this.firestore, 'users');
+    this.items$ = collectionData(itemCollection);
+  }
 
   user: User = new User();
   birthDate: string = new Date().toLocaleString('en-US');
 
-  saveUser() {
+  async saveUser() {
     this.changeDateForm();
     console.log('das ist unser neuer user:', this.user)
+
+    const aCollection = collection(this.firestore, 'users');
+    const usersDocRef = doc(aCollection);
+    await setDoc(usersDocRef, this.user.toJSON());
   }
 
   changeDateForm() {
--- CRM-BusinessApp/src/models/user.class.ts
+++ CRM-BusinessApp/src/models/user.class.ts
@@ -1,3 +1,5 @@
+import { CollectionReference, DocumentData } from "@angular/fire/firestore";
+
 export class User {
     firstName: string;
     lastName: string;
@@ -6,6 +8,7 @@ export class User {
     streetNo: string;
     zipCode: number;
     city: string;
+    id: string;
 
     constructor(obj?: any) {  // obj in constructor is nessescary for variables! 
         this.firstName = obj ? obj.firstName : '';   // ist eine if/else abfrage in kurzform; erstes element wird geprüft ob exist, wenn ja, dann wird obj.firstname eingegebn, wenn nicht, kommt leerer string!
@@ -15,7 +18,20 @@ export class User {
         this.streetNo = obj ? obj.streetNo : '';
         this.zipCode = obj ? obj.zipCode : '';
         this.city = obj ? obj.city : '';
+        this.id = obj ? obj.id : '';
     }
 
+    public toJSON() {
+        return {
+            firstName: this.firstName,
+            lastName: this.lastName,
+            birthDate: this.birthDate,
+            street: this.street,
+            streetNo: this.streetNo,
+            zipCode: this.zipCode,
+            city: this.city,
+            id: this.id
+        }
+    }
 
 }
